aae81266590ff05a1d7f2d93833edd0a113287e9
aLieexe <rizky.alay@gmail.com>
2025-01-15 08:08:08 +0700

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  daStyle,
+];
